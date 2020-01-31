106cd129a7579c9f06ff70d81e06179ae9009e05
QwQ2000 <396707050@qq.com>
2020-01-31 17:30:06 +0800

    spec2
    community&user&graph

--- design/page_links.py
+++ design/page_links.py
@@ -6,6 +6,7 @@ digraph {
     
     
     index [label="主页"]
+    user [label="用户"]
     domain_dashboard [label="领域控制台页面"]
     domain_select [label="新领域选择页面"]
     domain_intro [label="领域概况页面"]
@@ -14,14 +15,16 @@ digraph {
     test [label="测试页面"]
     graph_info [label="图表详细信息页面"]
     community [label="社区主页"]
-
+    new_thread [label="创建新讨论"]
+    thread [label="讨论"]
 
     {rank = same; learn; test;}
 
 
     index -> domain_dashboard
     index -> community
-    
+    index -> user
+
     domain_dashboard -> domain
     domain_dashboard -> domain_select
     domain_select -> domain_intro
@@ -29,7 +32,10 @@ digraph {
     domain -> learn
     domain -> test
     learn -> test
+    learn-> new_thread
     test -> learn
+    community -> new_thread
+    community -> thread
 }
 """
 
